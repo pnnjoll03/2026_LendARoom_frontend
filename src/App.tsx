9ca643337aaed08cda423a810d5dbb66f267277f
Panji Nayaka Reswara <PanjiNares09@gmail.com>
2026-02-13 21:41:40 +0700

    feat: mencoba mengambil data dari models menggunakan api

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+interface Room{
+  id: number;
+  name: string;
+  status: string;
+  capacity: number;
+  location: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const API_URL = "http://localhost:5187/api/Room";
+
+  useEffect(() => {
+    const fetchRoom = async() => {
+      try{
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setRooms(data);
+      }catch(error){
+        console.error("Gagal mengambil data:", error);
+      }finally{
+        setLoading(false);
+      }
+    };
+
+    fetchRoom();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Daftar Ruangan</h1>
+
+        {loading ? (
+          <p>Sedang memuat data...</p>
+        ) : (
+          <div style={{ display: 'grid', gap: '20px' }}>
+            { rooms.map((room) => (
+              <div key={room.id} style={{ border: '1px solid #ccc', padding: '10px',borderRadius: '8px'}}>
+                <h3>{ room.name }</h3>
+                <p>{ room.location }</p>
+                <p>Kapasitas: { room.capacity }</p>
+                <p>Status:  
+                  <span style={{ color: room.status === 'Tersedia' ? 'green' : 'red', fontWeight: 'bold'}}>
+                    { room.status }
+                  </span>
+                </p>
+              </div>
+            ))}
+          </div>
+        )
+      }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
